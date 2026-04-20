d07677543761c2b91a9378d2ae7ff58deafbff05
sinank-sinu <sinansinurr@gmail.com>
2026-04-20 17:06:34 +0530

    first commit

---
+++ models/vehicle_report_template_abstract.py
@@ -0,0 +1,57 @@
+
+from odoo import models, api
+
+class VehicleReportTemplateAbstract(models.AbstractModel):
+    _name = 'report.vehicle_repair_management.report_repair_details_template'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        form = data.get('form')
+
+        query = """
+        SELECT
+        vm.name AS model,
+        vr.vehicle_number,
+        rp.name AS customer,
+        advisor_partner.name AS advisor,
+        vr.start_date,
+        vr.deliver_date AS end_date,
+        vr.state,
+        vt.name AS vehicle_type,
+        vr.service_type,
+        vr.estimated_amount,
+        vr.total_parts_labor_cost AS total_amount
+        FROM vehicle_repair vr
+        LEFT JOIN fleet_vehicle_model vm ON vr.vehicle_model_id = vm.id
+        LEFT JOIN fleet_vehicle_model_category vt ON vr.vehicle_type_id = vt.id
+        LEFT JOIN res_partner rp ON vr.partner_id = rp.id
+        LEFT JOIN res_users ru ON vr.salesperson_id = ru.id
+        LEFT JOIN res_partner advisor_partner ON ru.partner_id = advisor_partner.id
+        WHERE vr.start_date >= %s
+        AND vr.start_date <= %s
+        """
+        params = [form['start_date'], form['end_date']]
+
+        if form.get('partner_ids'):
+            query += " AND vr.partner_id IN %s"
+            params.append(tuple(form['partner_ids']))
+
+        if form.get('salesperson_ids'):
+            query += " AND vr.salesperson_id IN %s"
+            params.append(tuple(form['salesperson_ids']))
+
+        self.env.cr.execute(query, params)
+        res = self.env.cr.dictfetchall()
+
+        partner_ids = form.get('partner_ids', [])
+        salesperson_ids = form.get('salesperson_ids', [])
+
+        return {
+        'docs': res,
+        'customer_count': len(partner_ids),
+        'advisor_count': len(salesperson_ids),
+        'single_customer': self.env['res.partner'].browse(partner_ids[0]).name
+        if len(partner_ids) == 1 else False,
+        'single_advisor': self.env['res.users'].browse(salesperson_ids[0]).name
+        if len(salesperson_ids) == 1 else False,
+        }
--- wizard/repair_report_wizard.py
+++ wizard/repair_report_wizard.py
@@ -1,32 +1,14 @@
-from odoo import models, fields,api
+from odoo import models, fields
 
 class RepairReportWizard(models.TransientModel):
     _name = 'repair.report.wizard'
-    _description = 'Repair Report Wizard'
+    _description = 'Vehicle Repair Report Wizard'
 
-    start_date = fields.Date(string="Start Date")
-    end_date = fields.Date(string="End Date")
-    partner_ids = fields.Many2many('res.partner', string="Customers")
-    salesperson_id = fields.Many2many('res.users', string="Service Advisors")
+    start_date = fields.Date(string='Start Date', required=True)
+    end_date = fields.Date(string='End Date', required=True)
+    partner_ids = fields.Many2many('res.partner', string='Customers')
+    salesperson_ids = fields.Many2many('res.users', string='Service Advisors')
 
     def action_print_report(self):
-        domain = []
-        if self.start_date:
-            domain.append(('create_date', '>=', self.start_date))
-        if self.end_date:
-            domain.append(('create_date', '<=', self.end_date))
-        if self.partner_ids:
-            domain.append(('partner_id', 'in', self.partner_ids.ids))
-        if self.salesperson_id:
-            domain.append(('salesperson_id', 'in', self.salesperson_id.ids))
-
-        repairs = self.env['vehicle.repair'].search(domain)
-
-        data = {
-            'form': self.read()[0],
-            'repairs': repairs.ids,
-            'multi_customer': len(self.partner_ids) > 1 or not self.partner_ids,
-            'multi_advisor': len(self.salesperson_id) > 1 or not self.salesperson_id,
-        }
-        return self.env.ref('vehicle_repair_management.action_report_repair_wizard').report_action(self, data=data)
-    
+       data = {'form': self.read()[0]}
+       return self.env.ref('vehicle_repair_management.action_report_repair_wizard' ).report_action(self, data=data)

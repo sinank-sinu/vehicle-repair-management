7abda1694cdb666d8b176454499ab3b057328947
sinank-sinu <sinansinurr@gmail.com>
2026-05-08 17:25:34 +0530

    first commit

--- controller/repair_controller.py
+++ controller/repair_controller.py
@@ -1,10 +1,16 @@
+
+# -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
 
 class SnippetController(http.Controller):
     @http.route('/repairs', type='json', auth="public", website=True)
     def get_top_repairs(self):
-        print("------- controller---")
-        repairs = request.env['vehicle.repair'].sudo().search_read([],limit=4)
-        print(repairs)
-        return repairs
+        """Fetch repair data and return it."""
+        repairs = request.env['vehicle.repair'].sudo().search_read([('id', '!=', None)],
+            fields=['partner_id', 'vehicle_type_id', 'vehicle_model_id', 'vehicle_number', 'image_1920', 'id'],
+            order='id DESC')
+        values = {
+            'repairs': repairs
+        }
+        return values
--- static/src/js/snippet.js
+++ static/src/js/snippet.js
@@ -2,15 +2,36 @@
 import { renderToElement } from "@web/core/utils/render";
 import publicWidget from "@web/legacy/js/public/public_widget";
 import { rpc } from "@web/core/network/rpc";
+
 publicWidget.registry.get_top_repairs = publicWidget.Widget.extend({
-   selector : '.top_repairs',
-    async willStart() {
-       const result = await rpc('/repairs', {});
-           console.log("------------------------nokkada muthee-----------------------")
+    selector: '.top_repairs',
+    async willStart()
+    {
+        const result = await rpc('/repairs', {});
 
-       if(result){
-           this.$target.empty().html(renderToElement('vehicle_repair_management.category_data', {result: result}))
-       }
-   },
+        if (result && result.repairs)
+        {
+            const data = result.repairs;
+            const chunkSize = 4;
+            const chunks = [];
+            for (let i = 0; i < data.length; i += chunkSize) {
+                chunks.push(data.slice(i, i + chunkSize));
+            }
+            if (chunks.length > 0)
+            {
+                chunks[0].is_active = true;
+            }
+            this.chunks = chunks;
+        }
+    },
+    start()
+    {
+        if (this.chunks)
+        {
+            const unique_id = "tempId_" + Math.random();
+            this.$target.empty().html(
+                renderToElement('vehicle_repair_management.category_data',
+                    {chunks: this.chunks, carousel_id: unique_id}));
+        }
+    },
 });
-
